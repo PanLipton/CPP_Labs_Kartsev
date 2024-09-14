bedaf2a87b221c6245262c9038287bb7aacbd362
PanLipton <panskillsasda@gmail.com>
2024-09-14 22:04:52 +0300

    Created the basic structure of laboratory work

---
+++ Lab3/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Lab3");
+        }
+    }
+}
---
+++ LabSelector/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+   namespace LabSelector
+   {
+       class Program
+       {
+           static void Main(string[] args)
+           {
+               while (true)
+               {
+                   Console.WriteLine("Виберіть лабораторну роботу (1-3) або 'q' для виходу:");
+                   string choice = Console.ReadLine();
+
+                   if (choice.ToLower() == "q")
+                   {
+                       break;
+                   }
+
+                   switch (choice)
+                   {
+                       case "1":
+                           Lab1.Program.Main(new string[] { });
+                           break;
+                       case "2":
+                           Lab2.Program.Main(new string[] { });
+                           break;
+                       case "3":
+                           Lab3.Program.Main(new string[] { });
+                           break;
+                       default:
+                           Console.WriteLine("Невірний вибір. Спробуйте ще раз.");
+                           break;
+                   }
+
+                Console.WriteLine("\nНатисніть Enter, щоб продовжити...");
+                Console.ReadLine();
+               }
+           }
+       }
+   }
